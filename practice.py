7da69f5c35630ca681a18a12b15561080768672c
siseldyke <siseldyke@gmail.com>
2025-03-03 16:18:49 -0500

    basic variable cheatsheet added

--- practice.py
+++ practice.py
@@ -1,2 +1,30 @@
-print("Hello world")
-print("double check")
+# print("Hello world")
+# print("double check")
+
+# variables: a container for a value. Behaves as the value it contains
+# print(type(name)) how to check data type of a variable
+
+# strings
+
+# first_name = "Collin"
+# last_name = "Jost"
+# full_name = first_name + " " + last_name
+# print("Hello "+full_name)
+
+# integers
+
+# age = 42
+# age += 1
+# print("Your age is: "+str(age)) 
+# # +str converts the int to a string
+
+# floats(decimals)
+
+# height = 250.5
+# print("Your height is: "+str(height)+"cm")
+# print(type(height))
+
+# Boolean(true or false)
+
+# human = True
+# print("are you a human: " +str(human))
