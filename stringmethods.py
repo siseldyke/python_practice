f962760ff73f409b4c9c8587bb3a687a84d4f119
siseldyke <siseldyke@gmail.com>
2025-03-10 12:48:45 -0400

    string methods completed

---
+++ stringmethods.py
@@ -0,0 +1,12 @@
+name = "dudecheck"
+
+# print(len(name))
+# print(name.find("u")) how to find a character in a string
+# print(name.capitalize()) make first letter capital
+# print(name.upper()) make string uppercase
+# print(name.lower()) make string lower case
+# print(name.isdigit()) checks for entire string to be numbers
+# print(name.isalpha())  checks for entire string to be alphabetical letters
+# print(name.count("d")) counts how many of asked letter etc are in the string
+# print(name.replace("d", "s")) replaces first arguement with second 
+# print(name*3) can multiply with python to show variable as many times as asked
