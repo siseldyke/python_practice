0f74fedee1fa9059e8f2cf93cf29e751579a1c78
siseldyke <siseldyke@gmail.com>
2025-03-10 13:34:09 -0400

    basics of user input added

---
+++ userinput.py
@@ -0,0 +1,11 @@
+name = input("What is your name?: ")
+age = int(input("How old are you?: "))
+height = float(input("How tall are you?: "))
+
+# use type cast to help with string concatination
+
+
+print("Hello " + name)
+
+print("You are " + str(age) + " years old")
+print("You are " +str(height) + "cm tall")
