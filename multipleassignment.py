38fb3c484aa9414dd429c05499b61aa43eebe793
siseldyke <siseldyke@gmail.com>
2025-03-10 12:39:34 -0400

    multiple assignment sheet added

---
+++ multipleassignment.py
@@ -0,0 +1,22 @@
+# name = "Bro"
+# age = 21
+# attractice = True
+
+# heres how to do it using one line:
+# name, age, attractive = "Bro", 21, True
+
+# print(name)
+# print(age)
+# print(attractive)
+
+# spongebob = 30
+# patrick = 30
+# sandy = 30
+# squidward = 30
+
+spongebob = patrick = sandy = squidward = 30
+
+print(spongebob)
+print(patrick)
+print(sandy)
+print(squidward)
