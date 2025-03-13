52a76f941ca1d8cd3667d0b5d676ad8cccce4ad5
siseldyke <siseldyke@gmail.com>
2025-03-13 16:20:22 -0400

    math functions with import added

---
+++ mathfunctions.py
@@ -0,0 +1,16 @@
+# import your math function from python
+import math
+
+pi = 3.14
+x = 1
+y = 2 
+z = 3
+
+# print(round(pi)) rounds to closest whole number
+# print(math.ceil(pi)) rounds UP to nearest integer
+# print(math.floor(pi)) rounds DOWN to nearest integer
+# print(abs(pi)) tells you how far away a number is from zero(absolute, abs for short)
+# print(pow(pi, 2)) raises number to the power of the second specificied number
+# print(math.sqrt(420)) square root function
+# print(max(x,y,z)) finds the highest variable within
+# print(min(x,y,z)) finds minimum value of variables
